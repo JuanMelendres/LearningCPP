cc4aa9dc2abc08475cee515b13714fbcf3e1a212
Juan Melendres <juanantoniomelendresvilla@MacBook-Air-de-Juan.local>
2017-05-22 18:38:04 -0500

    First Steps

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+// main() is where program execution begins.
+
+int main(){
+
+    cout << "Hello World\n"; // prints Hello World
+
+    return 0;
+
+}
